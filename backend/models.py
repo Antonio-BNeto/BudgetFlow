31276f3c26ae298d231c9cf3ba0d35a34b6d1de3
Antonio Barros <antonio.barros.alcantara.neto@ccc.ufcg.edu.br>
2025-08-19 21:56:19 -0300

    feat(models): create base models structure

---
+++ backend/models.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, Enum # Importa do SQLAlchemy os tipos e funções necessários para definir colunas de tabelas no banco de dados:
+from sqlalchemy.orm import relationship # Definir um relacionamento entre tabelas
+from db import Base  # Base declarativa do SQLAlchemy
+import enum # módulo do Python para criar tipos enumeradores
+
+class Category(Base): # Define a tabela de categorias
+
+    __tablename__ = 'categories'  # Nome da tabela no banco de dados
+    
+    id = Column(Integer, primary_key=True, index=True)  # Coluna de ID, chave primária e indexada
+    name = Column(String, unique=True, nullable=False)  # Coluna de nome da categoria, não pode ser nula e não repete
+
+    transactions = relationship("Transaction", back_populates="category")  # Relacionamento com a tabela de transações
+    budgets = relationship("Budget", back_populates="category")  # Relacionamento com a tabela de orçamentos
+ 
+class TransactionType(enum.Enum): # Define um tipo enumerador para os tipos de transações
+    income = "income" # Receita
+    expense = "expense" # Despesa
+
+class Transaction(Base): # Define a tabela de transações
+
+    __tablename__ = 'transactions'  # Nome da tabela no banco de dados
+    
+    id_transaction = Column(Integer, primary_key=True, index=True)  # Coluna de ID da transação, chave primária e indexada
+    type = Column(Enum(TransactionType), nullable=False)  # Coluna de tipo da transação, não pode ser nula
+    amount = Column(Float, nullable=False)  # Coluna de valor da transação, não pode ser nula
+    date = Column(Date, nullable=False)  # Coluna de data da transação, não pode ser nula
+    description = Column(String, nullable=True)  # Coluna de descrição da transação, pode ser nula
+    id_category = Column(Integer, ForeignKey('categories.id'), nullable=False)  # Chave estrangeira para a categoria, não pode ser nula
+    
+    category = relationship("Category", back_populates="transactions") # Relacionamento com a tabela de categorias
+
+class Budget(Base): # Define a tabela de orçamentos
+
+    __tablename__ = 'budgets' # Nome da tabela no banco de dados
+    
+    id_budget = Column(Integer, primary_key=True, index=True)  # Coluna de ID do orçamento, chave primária e indexada
+    month = Column(String, nullable=False)  # Coluna de mês do orçamento, não pode ser nula
+    limit = Column(Float, nullable=False)  # Coluna de limite do orçamento, não pode ser nula
+    id_category = Column(Integer, ForeignKey('categories.id'), nullable=False)  # Chave estrangeira para a categoria, não pode ser nula
+
+    category = relationship("Category", back_populates="budgets")  # Relacionamento com a tabela de categorias
